00e0b4006ebc0ea445c48f5c0f52eccb64795a30
Ka Chun <34387770+kachun333@users.noreply.github.com>
2021-03-15 22:08:38 +0800

    add fail test case

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -50,4 +50,17 @@ describe("Calculate", () => {
     cy.get('#calculate').click();
     cy.get('#result').should("have.text", "Result: 0.6666666666666666")
   });
+
+  it("purposely fail addition test", () => {
+    cy.visit("https://basic-calculator-by-angelol2046.netlify.app/");
+    cy.get("#valueA").click();
+    cy.get("#valueA").type("{backspace}");
+    cy.get("#valueA").type("2");
+    cy.get("#valueB").click();
+    cy.get("#valueB").type("{backspace}");
+    cy.get("#valueB").type("3");
+    cy.get("#add").click();
+    cy.get("#calculate").click();
+    cy.get('#result').should("have.text", "Result: 1")
+  });
 });
